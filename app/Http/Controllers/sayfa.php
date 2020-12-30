6c567355c8b8a38aa52c4f0d60eadba566692e21
RoberOz <roberrudi006@gmail.com>
2020-12-30 15:49:22 +0300

    Migrations, Models

--- app/Http/Controllers/sayfa.php
+++ app/Http/Controllers/sayfa.php
@@ -3,15 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use DB;
+use App\Book;
+use App\User;
 
-class request_methodu extends Controller
+class sayfa extends Controller
 {
   public function post(){
     //DB::table('users')->insert([
     //  'kullaniciadi'=>"rober",
     //  'sifre'=>123
     //]);
+
+    $books=Book::all();
+    echo $books;
+
+    $user = new User;
+    $user->username="rober";
+    $user->password=1234;
+    $user->save();
+
     return "post çalıştı";
   }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ Route::get('/', function () {
     return view('login');
 });
 
-Route::post('/login','request_methodu@post')->name('login.post');
+Route::post('/login','sayfa@post')->name('login.post');
