9342f283f4398a889baf59c1a02fba40d400a840
RoberOz <roberrudi006@gmail.com>
2021-01-02 18:11:00 +0300

    Migrations, Models, Seeds, Auth

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,19 @@
 //Route::get('/', function () {
 //    return view('login');
 //});
-Route::get('/','Homepage@index')->name('homepage');
 
-Route::post('/login','sayfa@post')->name('login.post');
+//Route::get('/','Homepage@index')->name('homepage');
 
-Route::get('/kitaplik',function(){
-  return view('kitaplik');
+//Route::post('/login','sayfa@post')->name('login.post');
+
+//Route::get('/kitaplik',function(){
+//  return view('kitaplik');
+//});
+
+Route::get('/',function(){
+  return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
