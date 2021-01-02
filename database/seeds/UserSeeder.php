d42558a705b0975c66509ccb9c6eee8e3881e47e
RoberOz <roberrudi006@gmail.com>
2021-01-02 19:29:45 +0300

    Migrations, Models, Seeders, Auth

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+          'username'=>'rober',
+          'password'=>1234
+        ]);
+
+        DB::table('books')->insert([
+          'name'=>'Metro 2033',
+          'author_name'=>'Dimitry',
+          'isbn_number'=>123456789,
+          'image_url'=>'asdf'
+        ]);
+
+        DB::table('books')->insert([
+          'name'=>'Fahrenheit 451',
+          'author_name'=>'Ray Bradbury',
+          'isbn_number'=>987654321,
+          'image_url'=>'qwerty'
+        ]);
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
-Route::get('/', function () {
-    return view('login');
-});
+//Route::get('/', function () {
+//    return view('login');
+//});
 
-Route::post('/login','sayfa@post')->name('login.post');
+//Route::post('/login','sayfa@post')->name('login.post');
+
+Auth::routes();
+
+Route::get('/', 'HomeController@index')->name('home');
