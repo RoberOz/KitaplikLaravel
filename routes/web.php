92bbe092e6d9d66bbe012f8018078ba0beb56e41
RoberOz <roberrudi006@gmail.com>
2021-01-06 12:24:21 +0300

    Login & Register, Sayfalar, Delete, Create, Edit, Middleware

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,10 @@
 //Route::post('/login','sayfa@post')->name('login.post');
 
 Route::get('/', function () {
-    return view('anasayfa');
+    return view('guest');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
 Route::resource('posts','CrudController');
